d95ca0539be22e9f5d41e506f29900e9927993bd
Knot <visarut.t@7solutions.co.th>
2025-01-31 01:52:31 +0700

    remove unuse

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -15,51 +15,8 @@ import UIKit
         let controller = window?.rootViewController as! FlutterViewController
         let registrar = controller.registrar(forPlugin: "ARViewFactory")
         registrar!.register(ARViewFactory(messenger: controller.binaryMessenger), withId: "ar_view")
-        //        methodChannel = FlutterMethodChannel(name: "onnx", binaryMessenger: controller.binaryMessenger)
-
-//        let modelPath = Bundle.main.path(forResource: "OULU_Protocol_2_model_0_0", ofType: "onnx")!
-//
-//        do {
-//            let ortEny = try ORTEnv(loggingLevel: .warning)
-//            ortSession = try ORTSession(env: ortEny, modelPath: modelPath, sessionOptions: nil)
-//        } catch {
-//            print(error);
-//        }
-//
-//
-//        methodChannel?.setMethodCallHandler { call, result in
-//            if call.method == "processImage" {
-//                guard let args = call.arguments as? [String: Any],
-//                      let imageData = args["image"] as? FlutterStandardTypedData
-//                else {
-//                    result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid image data", details: nil))
-//                    return
-//                }
-//
-//                self.runOnnxModel(imageData.data)
-//
-//            } else {
-//                result(FlutterMethodNotImplemented)
-//            }
-//        }
 
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
-
-    func runOnnxModel(_ imageData: Data) {
-        do {
-            let inputTensor = try ORTValue(tensorData: NSMutableData(data: imageData),
-                                           elementType: .float,
-                                           shape: [1, 3, 224, 224])
-            let outputs = try ortSession?.run(withInputs: ["input": inputTensor],
-                                              outputNames: ["output_pixel"],
-                                              runOptions: nil)
-
-            print(outputs)
-            methodChannel?.invokeMethod("processImage", arguments: outputs?.description ?? "")
-        } catch {
-            print(error)
-        }
     }
 }
